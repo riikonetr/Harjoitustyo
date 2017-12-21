6c6eb04eb189e11a0bc6b2ae6cfc4a285972193e
riikonetr <34094325+riikonetr@users.noreply.github.com>
2017-12-21 11:02:03 +0200

    Update DevelopmentProfile.java

--- Harjoitustyo/src/main/java/wad/config/DevelopmentProfile.java
+++ Harjoitustyo/src/main/java/wad/config/DevelopmentProfile.java
@@ -14,6 +14,22 @@ import wad.auth.JpaAuthenticationProvider;
 @EnableWebSecurity
 @Profile("development")
 public class DevelopmentProfile extends WebSecurityConfigurerAdapter {
+    
+    @Bean
+    public BasicDataSource dataSource() throws URISyntaxException {
+        URI dbUri = new URI(System.getenv("DATABASE_URL"));
+
+        String username = dbUri.getUserInfo().split(":")[0];
+        String password = dbUri.getUserInfo().split(":")[1];
+        String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
+
+        BasicDataSource basicDataSource = new BasicDataSource();
+        basicDataSource.setUrl(dbUrl);
+        basicDataSource.setUsername(username);
+        basicDataSource.setPassword(password);
+
+        return basicDataSource;
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
