08ad256cda22b741fb4fa87e22f19f699d9be5f9
riikonetr <34094325+riikonetr@users.noreply.github.com>
2017-12-21 11:24:34 +0200

    Update DevelopmentProfile.java

--- Harjoitustyo/src/main/java/wad/config/DevelopmentProfile.java
+++ Harjoitustyo/src/main/java/wad/config/DevelopmentProfile.java
@@ -2,22 +2,15 @@ package wad.config;
 import java.net.URI;
 import java.net.URISyntaxException;
 import org.apache.commons.dbcp2.BasicDataSource;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.authentication.configurers.GlobalAuthenticationConfigurerAdapter;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import wad.auth.JpaAuthenticationProvider;
 
 @Configuration
 @EnableWebSecurity
 @Profile("development")
-public class DevelopmentProfile extends WebSecurityConfigurerAdapter {
+public class DevelopmentProfile {
     
     @Bean
     public BasicDataSource dataSource() throws URISyntaxException {
@@ -33,40 +26,5 @@ public class DevelopmentProfile extends WebSecurityConfigurerAdapter {
         basicDataSource.setPassword(password);
 
         return basicDataSource;
-    }
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .antMatchers("/login", "/signup", "/static*/**").permitAll()
-                .antMatchers(HttpMethod.POST, "/persons").permitAll()
-                .anyRequest().authenticated();
-
-        http.formLogin()
-                .loginPage("/login")
-                .loginProcessingUrl("/authenticate")
-                .defaultSuccessUrl("/index")
-                .usernameParameter("username")
-                .passwordParameter("password")
-                .permitAll();
-
-        http.logout()
-                .logoutUrl("/logout")
-                .logoutSuccessUrl("/login")
-                .permitAll()
-                .invalidateHttpSession(true);
-
-    }
-
-    @Configuration
-    protected static class AuthenticationConfiguration extends GlobalAuthenticationConfigurerAdapter {
-
-        @Autowired
-        private JpaAuthenticationProvider jpaAuthenticationProvider;
-
-        @Override
-        public void init(AuthenticationManagerBuilder auth) throws Exception {
-            auth.authenticationProvider(jpaAuthenticationProvider);
-        }
     }
 }
